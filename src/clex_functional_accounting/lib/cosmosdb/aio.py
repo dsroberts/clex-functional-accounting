2b236067db166e1e71a4c806f8572ea7300da6b2
Dale Roberts <dsroberts@unimelb.edu.au>
2023-10-03 13:59:55 +1100

    Await it instead

--- src/clex_functional_accounting/lib/cosmosdb/aio.py
+++ src/clex_functional_accounting/lib/cosmosdb/aio.py
@@ -74,8 +74,7 @@ class CosmosDBWriter():
         if not pk:
             pk = self._get_partition_key_val(container)
 
-        ### This is a future
-        return container_client.delete_item(d,pk)
+        await container_client.delete_item(d,pk)
 
     async def read_items(self, container: str, item: Any, field: Optional[str] =  None, partition_key_val: Optional[str] = None, once_off: bool = False) -> List[Dict[str,Any]]:
 
